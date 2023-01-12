6a6ea09fdd27065d3f2b20104d0828789f0a76ab
gabr <blabla@gmail.com>
2023-01-12 18:37:03 +0100

    test

--- loss.py
+++ loss.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
 class DiceBCELoss(nn.Module):
     def __init__(self, alpha=0.5, weight=None, size_average=True):
         super(DiceBCELoss, self).__init__()
@@ -12,15 +13,26 @@ class DiceBCELoss(nn.Module):
     def forward(self, inputs, targets, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)
-        
+        #inputs = F.sigmoid(inputs)
+
+        binary_inputs = torch.argmax(inputs, keepdim = True)
+
         #flatten label and prediction tensors
         inputs = inputs.reshape(-1)
         targets = targets.reshape(-1)
         
-        intersection = (inputs * targets).sum()                            
-        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        intersection = torch.mul(binary_inputs , targets).sum()                       
+        dice_loss = 1 - (2.*intersection + smooth)/(binary_inputs.sum() + targets.sum() + smooth) 
+
+        print(intersection)
+
         BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        Dice_BCE = self.alpha*BCE + self.alpha*dice_loss
+        print(binary_inputs.sum())
+        print(targets.sum())
+        print("BCE")
+        print(BCE)
+        print(dice_loss)
+        #Dice_BCE = self.alpha*BCE + self.alpha*dice_loss
+        Dice_BCE = BCE
         
         return Dice_BCE
